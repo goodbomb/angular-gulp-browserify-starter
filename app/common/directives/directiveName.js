8f3345621c7bbcc463efa46530ae1dfce396aae0
Ben Scott <ben@goodbomb.com>
2014-06-23 12:44:02 -0700

    Added common folders

---
+++ app/common/directives/directiveName.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function directiveName() {
+	return {
+		template: require(''),
+		restrict: 'EA',
+		scope: true
+	};
+};
---
+++ app/common/resources/resourceName.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function resourceName() {
+	return {
+		
+	};
+};
