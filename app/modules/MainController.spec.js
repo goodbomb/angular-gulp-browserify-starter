ee86bb9c47676685db2b32bb3380c286b1343fb3
goodbomb <ben@goodbomb.com>
2015-04-17 16:01:59 -0700

    Added working test for MainController

--- app/modules/MainController.spec.js
+++ app/modules/MainController.spec.js
@@ -1,32 +1,27 @@
-//jscs:disable requireSpaceAfterLineComment
-//jscs:disable requireCapitalizedComments
 /*jshint expr: true*/
 
 'use strict';
-
-// var MainCtrl = require('./MainController');
 
 describe('MainController', function() {
 
     var ctrl, scope;
 
-    beforeEach(function() {
-        // ctrl = new MainController();
-        // angular.mock.module('modules');
+    beforeEach(angular.mock.module('modules'));
 
-        // // mock the controller
-        angular.mock.inject(function($rootScope, $controller) {
+    beforeEach(function() {
+
+        angular.mock.inject(function ($controller, $rootScope) {
             scope = $rootScope.$new();
-            ctrl = $controller('MainCtrl', {$scope: scope});
+            ctrl = $controller('MainCtrl', {
+                $scope: scope
+            });
         });
+
     });
 
 
     it('should exist', function() {
-        // expect(ctrl).to.not.be.undefined;
-        expect(true).to.be.true;
+        expect(ctrl).to.not.be.undefined;
     });
 
 });
-
-console.log('is this working?');
--- karma.conf.js
+++ karma.conf.js
@@ -1,73 +1,74 @@
 // Karma configuration
 
-/*jshint node:true*/
-
 'use strict';
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
+    config.set({
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify', 'mocha', 'chai', 'sinon'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: './',
 
-    // list of files / patterns to load in the browser
-    files: [
-      './libs/angular/angular.js',
-      './libs/angular-mocks/angular-mocks.js', // for angular.mock.module and inject.
-      './app/**/*.js',
-      './app/**/*.spec.js'
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['browserify', 'mocha', 'chai', 'sinon'],
 
-    // list of files to exclude
-    exclude: [],
+        // list of files / patterns to load in the browser
+        files: [
+            './libs/angular/angular.js',
+            './libs/angular-mocks/angular-mocks.js', // for angular.mock.module and inject.
+            './app/**/*.js',
+            './app/**/*.spec.js'
+        ],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      // './dist/bundle.js': ['browserify'],
-      './app/**/*.js': ['browserify']
-    },
+        // list of files to exclude
+        exclude: [],
 
-    // karma-browserify configuration
-    browserify: {
-      debug: true,
-      transform: ['debowerify'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            // './dist/bundle.js': ['browserify'],
+            './app/**/*.js': ['browserify']
+        },
 
-      // don't forget to register the extensions
-      extensions: ['.js']
-    },
+        // karma-browserify configuration
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        browserify: {
+            debug: true,
+            transform: ['debowerify'],
 
-    // web server port
-    port: 9876,
+            // don't forget to register the extensions
+            extensions: ['.js']
+        },
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // web server port
+        port: 9876,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-      // 'Chrome',
-      'PhantomJS'
-    ],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: [
+            // 'Chrome',
+            'PhantomJS'
+        ],
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+
+    });
 };
