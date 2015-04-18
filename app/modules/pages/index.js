ad9851ffad8df8679a30c732395e78748f6d9ab6
goodbomb <ben@goodbomb.com>
2015-04-17 18:16:30 -0700

    Added a navbar and basic template styling

--- app/modules/pages/index.js
+++ app/modules/pages/index.js
@@ -1,5 +1,7 @@
 'use strict';
 // Home View
-module.exports = angular.module('modules.pages', [])
-    .controller('PagesCtrl', require('./PagesController'))
-    .config(require('./pagesConfig'));
+module.exports = angular.module('modules.pages', [
+        require('./about').name
+    ])
+    .config(require('./pagesConfig'))
+    .controller('PagesCtrl', require('./PagesController'));
--- app/modules/pages/pagesConfig.js
+++ app/modules/pages/pagesConfig.js
@@ -9,8 +9,8 @@ function pageRoutes($stateProvider) {
         data: {
             moduleClasses: 'page',
             pageClasses: 'about',
-            pageTitle: 'How it Works',
-            pageDescription: 'Find your favourite cause, donate and receive vouchers of equal value that are redeemable at participating businesses.'
+            pageTitle: 'About',
+            pageDescription: 'Some description.'
         }
     };
 
