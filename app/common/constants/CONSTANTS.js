200ac4efcc40c247c7e47e04a30dd37f50b709d2
goodbomb <ben@goodbomb.com>
2015-04-19 09:03:32 -0700

    Added common/constants

---
+++ app/common/constants/CONSTANTS.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var CONSTANTS = (function() {
+    return {
+        successMessage: 'You have successfully logged in.',
+        failureMessage: 'Your username or password is incorrect.',
+        key: 'value'
+    };
+}());
+
+CONSTANTS.$inject = [];
+module.exports = CONSTANTS;
